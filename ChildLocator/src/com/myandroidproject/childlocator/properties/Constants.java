40fbd0899c3a459cd689e0926d39a641a833d502
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2014-12-17 17:54:23 +0530

    addong depedent

--- ChildLocator/src/com/myandroidproject/childlocator/properties/Constants.java
+++ ChildLocator/src/com/myandroidproject/childlocator/properties/Constants.java
@@ -11,6 +11,10 @@ public class Constants {
 	public static String DBError ="Unale to insert DBError";
 	public static String Username_Password_Null ="Username or Password cannot be null";
 	public static String Username_Password_length="UserName and Password must contain minimum 6 letters"; 
+	public static final String ADD_Dependent = "http://10.0.2.2:9999/ChildTracker_php/webService/DependentService.php";
+	public static final String Service_URL = "http://10.0.2.2:9999/ChildTracker_php/webService/UpdateService.php";
+	
+	
 	private Constants() {
 		
 		
